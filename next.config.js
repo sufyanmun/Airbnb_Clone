27b9c46547e0fde70a95acd26e99e1b8df5cd5af
sufyanmun <sufyanmun@outlook.com>
2023-01-02 00:34:56 -0500

    Airbnb-clone

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = { nextConfig,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'logodownload.org',
+        port: '',
+        pathname: '/wp-content/uploads/2016/10/airbnb-logo-10.png',
+      },
+    ],
+  },
+};
